2c37322b62566adbdc7882a6eeaa9282c67739b8
Jan Houben <jan@nexttrex.de>
2017-01-03 17:07:48 +0100

    fixed unused migrator test

--- tests/ettc/Database/Migration/testMigrations/004_addASimpleTable4.php
+++ tests/ettc/Database/Migration/testMigrations/004_addASimpleTable4.php
@@ -5,12 +5,12 @@ class addASimpleTable4 extends AbstractMigration
 	public function upgrade()
 	{
 		$sql = 'CREATE TABLE `simpleTable4` ( `id` INT(255) NULL )';
-		return $this->db->query($sql);
+		return $this->db->getPdo()->prepare($sql)->execute();
 	}
 
 	public function downgrade()
 	{
 		$sql = 'DROP TABLE `simpleTable4`';
-		return $this->db->query($sql);
+		return $this->db->getPdo()->prepare($sql)->execute();
 	}
 }
