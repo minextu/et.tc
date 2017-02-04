04055ae3ee62ca8b2500926430a60320d7115d73
Jan Houben <jan@nexttrex.de>
2017-02-04 22:21:18 +0100

    fix EttcApi on installations in root folder

--- api.php
+++ api.php
@@ -7,5 +7,5 @@ require_once("src/autoload.php");
 session_start();
 
 $ettc = new Ettc;
-$rootDir = dirname($_SERVER['SCRIPT_NAME']) . "/api";
+$rootDir = rtrim(dirname($_SERVER['SCRIPT_NAME']), "/") . "/api";
 EttcApi::run($rootDir, $ettc->getDb());
