4f3c2999c350f6b33b55d2b75aa55b8225e46e6c
Jan Houben <jan@nexttrex.de>
2017-04-07 15:29:29 +0200

    ci: set timezone

--- ci/dockerInstall.sh
+++ ci/dockerInstall.sh
@@ -13,6 +13,10 @@ docker-php-ext-install pdo_mysql
 docker-php-ext-install zip
 pecl install xdebug && docker-php-ext-enable xdebug
 
+# set timezone
+echo "[Date]" >> /usr/local/etc/php/php.ini
+echo "date.timezone = UTC" >> /usr/local/etc/php/php.ini
+
 # Install phpunit
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-5.7.phar
 chmod +x /usr/local/bin/phpunit
